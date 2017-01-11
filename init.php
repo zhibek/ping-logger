844e7cc5a12b7379f843ddae6a5419718207f467
John Levermore <john.levermore@zhibek.com>
2017-01-11 13:49:24 +0000

    Check for existence of config.php and exit with error when missing.

--- init.php
+++ init.php
@@ -2,6 +2,10 @@
 
 date_default_timezone_set('Africa/Cairo');
 
+if (!is_file('config.php')) {
+    exit('Config file "config.php" is required!' . PHP_EOL);
+}
+
 require('src/PingLogger.php');
 
 require('config.php');
